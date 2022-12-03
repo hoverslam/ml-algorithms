27d5ccff7caf5e36473eab41169a0169e4bda418
hoverslam <hofmann.christopher@outlook.com>
2022-12-03 18:45:28 +0100

    Add doc strings

--- utils/metrics.py
+++ utils/metrics.py
@@ -2,4 +2,13 @@ import numpy as np
 
 
 def accuracy_score(y_true:np.ndarray, y_pred:np.ndarray) -> float:
+    """Calculate the percentage of correct predictions.
+
+    Args:
+        y_true (np.ndarray): Correct labels.
+        y_pred (np.ndarray): Predicted labels.
+
+    Returns:
+        float: Fraction of correctly classified samples.
+    """
     return np.mean(y_pred == y_true)
