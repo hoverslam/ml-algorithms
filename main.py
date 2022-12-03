9a953f30ab2dbae2148deca4cb1bc59f602653fa
hoverslam <hofmann.christopher@outlook.com>
2022-12-03 18:46:08 +0100

    Train perceptron

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
-import numpy as np
+from utils.datasets import generate_data, split_data
+from utils.metrics import accuracy_score
 
-from utils.datasets import generate_data, split_data, plot_data
-from support_vector_machines.svm import SVM
-
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
+from neural_networks.perceptron import SLP
 
 
 if __name__ == "__main__":
-    X, y = generate_data(lin_sep=False)
+    X, y = generate_data(1000, type="linear")
     X_train, X_test, y_train, y_test = split_data(X, y)
-    # plot_data(X, y)
     
-    # my implementation
-    clf = SVM(kernel="poly")
+    clf = SLP()
     clf.fit(X_train, y_train)
-    y_pred = clf.predict(X_test)
-    
-    # sklearn baseline
-    clf_baseline = SVC(kernel="poly", degree=2)
-    clf_baseline.fit(X_train, y_train)
-    y_baseline_pred = clf_baseline.predict(X_test)
-    
-    print(f"Accuracy: {accuracy_score(y_test, y_pred)}")
-    print(f"Accuracy (baseline): {accuracy_score(y_test, y_baseline_pred)}")
+    train_acc = accuracy_score(y_train, clf.predict(X_train))
+    test_acc = accuracy_score(y_test, clf.predict(X_test))
+    print(f"Accuracy (train, test): {train_acc}, {test_acc}")
