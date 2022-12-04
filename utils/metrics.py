1f417869470b7869829152e7c0c9b33cb268c629
hoverslam <hofmann.christopher@outlook.com>
2022-12-04 10:39:04 +0100

    Add MSE, RMSE, and MAE

--- utils/metrics.py
+++ utils/metrics.py
@@ -12,3 +12,31 @@ def accuracy_score(y_true:np.ndarray, y_pred:np.ndarray) -> float:
         float: Fraction of correctly classified samples.
     """
     return np.mean(y_pred == y_true)
+
+def mean_squared_error(y_true:np.ndarray, y_pred:np.ndarray, root:bool=False) -> float:
+    """Calculate the average squared difference between the estimated values and the actual value.
+
+    Args:
+        y_true (np.ndarray): Correct labels.
+        y_pred (np.ndarray): Predicted labels.
+        root (bool, optional): If set to True the RMSE is calculated. Defaults to False.
+
+    Returns:
+        float: Mean squared error (MSE). If root=True the root mean squared error (RMSE) is returned.
+    """
+    if root:
+        return np.sqrt(np.mean((y_pred - y_true)**2))
+    else: 
+        return np.mean((y_pred - y_true)**2)
+
+def mean_absolute_error(y_true:np.ndarray, y_pred:np.ndarray) -> float:
+    """Calculate arithmetic average of the absolute errors.
+
+    Args:
+        y_true (np.ndarray): Correct labels.
+        y_pred (np.ndarray): Predicted labels.
+
+    Returns:
+        float: Mean absolute error (MAE).
+    """
+    return np.mean(np.abs(y_pred - y_true))
