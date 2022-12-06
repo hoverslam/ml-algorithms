5af254b1dc78364bf7ad3fdda4941a4ff53c4a2d
hoverslam <hofmann.christopher@outlook.com>
2022-12-06 11:00:01 +0100

    Small changes

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ from neural_network.perceptron import MLP
 
 
 if __name__ == "__main__":
-    X, y = generate_data(1000, type="clusters")
+    X, y = generate_data(10000, type="clusters")
     X_train, X_test, y_train, y_test = split_data(X, y)
 
-    clf = MLP(X.shape[1], (100,), epochs=500)
-    clf.fit(X_train, y_train)
-    train_acc = clf.score(X_train, y_train)
-    test_acc = clf.score(X_test, y_test)
-    print(f"Accuracy (train, test): {train_acc}, {test_acc}")
+    clf1 = MLP(X.shape[1], (100, ), epochs=100)
+    clf1.fit(X_train, y_train, plot_history=True)
+    train_acc = clf1.score(X_train, y_train)
+    test_acc = clf1.score(X_test, y_test)
+    print(f"Accuracy [clf1]: {train_acc:.4f} (train), {test_acc:.4f} (test)")
 
-    clf = MLPClassifier()
-    clf.fit(X_train, y_train)
-    train_acc = clf.score(X_train, y_train)
-    test_acc = clf.score(X_test, y_test)
-    print(f"Accuracy (train, test): {train_acc}, {test_acc}")
+    ## Scikit-learn implementation
+    clf2 = MLPClassifier()
+    clf2.fit(X_train, y_train)
+    train_acc = clf2.score(X_train, y_train)
+    test_acc = clf2.score(X_test, y_test)
+    print(f"Accuracy [clf2]: {train_acc:.4f} (train), {test_acc:.4f} (test)")
