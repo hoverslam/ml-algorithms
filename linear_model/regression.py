866b272dd840ac1cf3a9c78bdda3f0f2e5d7d97b
hoverslam <hofmann.christopher@outlook.com>
2022-12-29 19:24:01 +0100

    Add Ridge regression

--- linear_model/regression.py
+++ linear_model/regression.py
@@ -49,3 +49,60 @@ class LinearRegression:
             float: R squared (R^2).
         """
         return r2_score(y, self.predict(X))
+
+
+class RidgeRegression:
+    """Implements Ridge regression using OLS estimator.
+    """
+    
+    def __init__(self, alpha:float=1.0) -> None:
+        """Initialize Ridge regression.
+
+        Args:
+            alpha (float, optional): L2 term, controlling regularization strength. 
+                Defaults to 1.0.
+        """
+        self.alpha = alpha
+        
+    def fit(self, X:np.ndarray, y:np.ndarray) -> None:
+        """Fit a regression line to the training data.
+
+        Args:
+            X (np.ndarray): The input samples.
+            y (np.ndarray): The target values.
+        """
+        n_samples, n_features = X.shape
+        y = y.reshape((n_samples, 1))
+        
+        # Add bias to feature matrix
+        input = np.hstack((np.ones((1, n_samples)).T, X))
+
+        # OLS estimator: https://en.wikipedia.org/wiki/Ridge_regression
+        gram_matrix = np.dot(input.T, input) + self.alpha * np.eye(n_features+1)
+        self.weights = np.linalg.inv(gram_matrix).dot(input.T).dot(y)
+    
+    def predict(self, X:np.ndarray) -> np.ndarray:
+        """Make predictions on a given feature matrix.
+
+        Args:
+            X (np.ndarray): The input samples.
+
+        Returns:
+            np.ndarray: Predicted values.
+        """
+        # Add bias to feature matrix
+        input = np.hstack((np.ones((1, X.shape[0])).T, X))
+        
+        return np.dot(input, self.weights).reshape(-1)
+    
+    def score(self, X:np.ndarray, y:np.ndarray) -> float:
+        """Caclulate the R2 score for a given feature matrix and the true target values.
+
+        Args:
+            X (np.ndarray): The input samples.
+            y (np.ndarray): The target values.
+
+        Returns:
+            float: R squared (R^2).
+        """
+        return r2_score(y, self.predict(X))
